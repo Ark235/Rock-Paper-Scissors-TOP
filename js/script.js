ed2eb4b4ddfba168523f82499dc4653a359b5fcc
Arkady <75026162+Ark235@users.noreply.github.com>
2022-07-13 17:44:00 +0500

    Rewrite everything, it works somehow, but don't seems right

--- js/script.js
+++ js/script.js
@@ -1,91 +1,59 @@
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
 
+btnRock.addEventListener('click', () => newCombatEntry(btnRock.classList[1]));
+btnPaper.addEventListener('click', () => newCombatEntry(btnPaper.classList[1]));
+btnScissors.addEventListener('click', () => newCombatEntry(btnScissors.classList[1]));
 
+/** Generate computer's turn data */
 
-// Generate rock/paper/scissors for computer to play
-
-function computerPlay() {
+function computerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     switch (randomNum) {
         case 1:
-            return "rock";
+            return 'rock';
             break;
         case 2:
-            return "paper";
+            return 'paper';
             break;
         case 3:
-            return "scissors";
+            return 'scissors';
             break;
         default:
-            computerPlay();
+            computerChoice();
     }
 }
 
-// Ask for player rock/paper/scissors choice
+/** Check who win 1 round */
 
-function playerPlay() {
-    let playerInput = prompt("Rock, paper or scissors?").toLowerCase();
-    if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
-        return playerInput;
-    } else {
-        playerInput = 0;
-        return playerInput;
-    }
+function checkWinner(playerChoice, computerChoice) {
+    let winner;
+    if (playerChoice === computerChoice) {
+        winner = 0;
+    } else if ((playerChoice == 'rock' && computerChoice != 'paper')
+        || (playerChoice == 'paper' && computerChoice != 'scissors')
+        || (playerChoice == 'scissors' && computerChoice != 'rock')) {
+        winner = 1;
+    } else winner = 2;
+    return winner;
 }
 
-// Play round of the game
+/** Add new entry to the combat log */
 
-function playRound(playerSelection = playerPlay(), computerSelection = computerPlay()) {
-    if (playerSelection != 0) {
-        let roundWinner;
-        alert(playerSelection + " VS " + computerSelection);
-        if (playerSelection === computerSelection) {
-            roundWinner = 0;                                                    // No one wins. Tie. 0
-        } else if ((playerSelection === "rock" && computerSelection != "paper")
-            || (playerSelection === "scissors" && computerSelection != "rock")
-            || (playerSelection === "paper" && computerSelection != "scissors")) {
-            roundWinner = 1;                                                    // Player wins. 1
-        } else {
-            roundWinner = 2;                                                    // Computer wins. 2
-        }
-        if (roundWinner == 1) {
-            alert("You won, " + playerSelection + " beats " + computerSelection);
-        } else if (roundWinner == 2) {
-            alert("Computer won this round, " + computerSelection + " beats " + playerSelection);
-        } else alert("It's a tie!");
-        return roundWinner;
-    } else return -1;                                                           // Wrong input
-}
-
-// Play the game
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 1; i <= 5; i++) {
-        alert(`It's ${i} round!`);
-        let counter = playRound();
-
-        // Add to score
-
-        if (counter == 1) {
-            playerScore++;
-        } else if (counter == 2) {
-            computerScore++;
-        } else if (counter == -1) {
-            alert("Wrong input. Refresh this page or type 'game()' in the console to begin anew.");
+function newCombatEntry(playerChoice) {
+    const combatLog = document.querySelector('.combat-log');
+    const newLogEntry = document.createElement('p');
+    switch (checkWinner(playerChoice, computerChoice())) {
+        case 1:
+            newLogEntry.textContent = `You win this round!`;
             break;
-        }
-
-        // Check the score
-
-        if (i > 3 && (computerScore > playerScore)) {
-            alert(`Computer wins with score ${computerScore + " : " + playerScore}`);
+        case 2:
+            newLogEntry.textContent = `Computer wins this round!`;
             break;
-        } else if (i > 3 && (computerScore < playerScore)) {
-            alert(`You win with score ${playerScore + " : " + computerScore}`);
+        case 0:
+            newLogEntry.textContent = `No one wins, it\'s a tie`;
             break;
-        } else if (i > 3 && (computerScore == playerScore)) {
-            alert(`It's a tie! Score is ${"Computer " + computerScore + " : " + playerScore + " Player"}`);
-        }
     }
+    combatLog.appendChild(newLogEntry);
 }
